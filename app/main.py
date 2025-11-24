f1aca70ee7802ba32b42b95b03f12953e6626bb5
Oleg <bagrijoleh@gmail.com>
2025-11-24 13:48:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     def wrapper(*args, **kwargs) -> Callable:
-        key = tuple(args)
+        key = args + tuple(sorted(kwargs.items()))
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
